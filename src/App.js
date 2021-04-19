88365c85aa1238662ffbf7ee884d00c9987ed7a1
Arwa Ali <aaa297@cornell.edu>
2021-04-19 16:43:10 -0400

    updated api

--- src/App.js
+++ src/App.js
@@ -21,17 +21,79 @@ function App() {
     getTasks()
   }, [])
 
-  // // Fetch Tasks
-  // const fetchTasks = async () => {
-  //   const res = await fetch('http://localhost:5000/tasks')
+  // // // Fetch Tasks
+  // // const fetchTasks = async () => {
+  // //   const res = await fetch('http://localhost:5000/tasks')
+  // //   const data = await res.json()
+
+  // //   return data
+  // // }
+
+  // // Fetch Task
+  // const fetchTask = async (id) => {
+  //   const res = await fetch(`http://localhost:5000/tasks/${id}`)
   //   const data = await res.json()
 
   //   return data
   // }
 
-  // Fetch Tasks
+  // // Add Task
+  // const addTask = async (task) => {
+  //   const res = await fetch('http://localhost:5000/tasks', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-type': 'application/json',
+  //     },
+  //     body: JSON.stringify(task),
+  //   })
+
+  //   const data = await res.json()
+
+  //   setTasks([...tasks, data])
+  //   // const id = Math.floor(Math.random() * 10000) + 1
+  //   // const newTask = { id, ...task }
+  //   // setTasks([...tasks, newTask])
+  // }
+
+  // // Delete Task
+  // const deleteTask = async (id) => {
+  //   await fetch(`http://localhost:5000/tasks/${id}`, {
+  //     method: 'DELETE',
+  //   })
+
+  //   setTasks(tasks.filter((task) => task.id !== id))
+  // }
+
+  // // Toggle Reminder
+  // const toggleReminder = async (id) => {
+  //   const taskToToggle = await fetchTask(id)
+  //   const updatedTask = {
+  //     ...taskToToggle,
+  //     reminder: !taskToToggle.reminder
+  //   }
+
+  //   const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+  //     method: 'PUT',
+  //     headers: {
+  //       'Content-type': 'application/json',
+  //     },
+  //     body: JSON.stringify(updatedTask),
+  //   })
+
+  //   const data = await res.json()
+
+  //   setTasks(
+  //     tasks.map((task) =>
+  //       task.id === id ? {
+  //         ...task, reminder: data.reminder
+  //       } : task
+  //     )
+  //   )
+  // }
+
+   // Fetch Tasks
   const fetchTasks = async () => {
-    const res = await fetch('https://my-task-tracking.herokuapp.com//tasks')
+    const res = await fetch('https://my-task-tracking.herokuapp.com/tasks')
     const data = await res.json()
 
     return data
@@ -39,64 +101,10 @@ function App() {
 
   // Fetch Task
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const res = await fetch(`https://my-task-tracking.herokuapp.com/tasks/${id}`)
     const data = await res.json()
 
     return data
-  }
-
-  // Add Task
-  const addTask = async (task) => {
-    const res = await fetch('http://localhost:5000/tasks', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(task),
-    })
-
-    const data = await res.json()
-
-    setTasks([...tasks, data])
-    // const id = Math.floor(Math.random() * 10000) + 1
-    // const newTask = { id, ...task }
-    // setTasks([...tasks, newTask])
-  }
-
-  // Delete Task
-  const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'DELETE',
-    })
-
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
-
-  // Toggle Reminder
-  const toggleReminder = async (id) => {
-    const taskToToggle = await fetchTask(id)
-    const updatedTask = {
-      ...taskToToggle,
-      reminder: !taskToToggle.reminder
-    }
-
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(updatedTask),
-    })
-
-    const data = await res.json()
-
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? {
-          ...task, reminder: data.reminder
-        } : task
-      )
-    )
   }
 
   return (
