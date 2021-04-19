ab3b6c7b4bb6f911b6adcc9168fdac56529033f9
Arwa Ali <aaa297@cornell.edu>
2021-04-19 16:53:17 -0400

    updated api

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ function App() {
   //   )
   // }
 
-   // Fetch Tasks
+  // Fetch Tasks
   const fetchTasks = async () => {
     const res = await fetch('https://my-task-tracking.herokuapp.com/tasks')
     const data = await res.json()
@@ -105,6 +105,60 @@ function App() {
     const data = await res.json()
 
     return data
+  }
+
+  // Add Task
+  const addTask = async (task) => {
+    const res = await fetch('https://my-task-tracking.herokuapp.com/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = async (id) => {
+    await fetch(`https://my-task-tracking.herokuapp.com/tasks/${id}`, {
+      method: 'DELETE',
+    })
+
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = {
+      ...taskToToggle,
+      reminder: !taskToToggle.reminder
+    }
+
+    const res = await fetch(`https://my-task-tracking.herokuapp.com/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updatedTask),
+    })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? {
+          ...task, reminder: data.reminder
+        } : task
+      )
+    )
   }
 
   return (
